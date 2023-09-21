1e479187e38e705f29f07bb6a1a45f62c2db6f0c
Ryan Drachenberg <tssinvestments@gmail.com>
2023-09-21 12:08:22 -0400

    moved testimonial imgs around

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,7 +5,6 @@ import { CheckBadgeIcon, Cog6ToothIcon, CurrencyDollarIcon, ChartBarIcon, Cursor
 const Testimonials = () => {
     return (
         <div>
-            
             <div className="border-4 rounded-1xl  h-20 z-0">
                 <div className="mx-auto text-center md:max-w-xl lg:max-w-3xl sm:max-w-sm z-10">
                     <div className="mb-6 text-2xl sm:text-3xl pt-6 font-bold  text-neutral-800 dark:text-black z-20">
@@ -53,10 +52,26 @@ const Testimonials = () => {
 
                 </ul>
             </div>
-
-                
-                <div className="grid gap-6 text-center md:grid-cols-3 lg:gap-12 mt-6">
-                
+            <div className="grid gap-6 text-center md:grid-cols-3 lg:gap-12 mt-6">
+                <div className="mb-1 md:mb-0">
+                    <div className="mb-6 flex justify-center">
+                    <Image width={100} height={100} src="/misc/alex-h-rounded.png" className="w-32 max-h-[131.33px] bg-cover bg-center rounded-full shadow-lg dark:shadow-black/30" alt='testimonial person three'/>
+                    </div>
+                    <h5 className="mb-4 text-xl font-semibold">Alex H.</h5>
+                    <p className="mb-4 text-lg">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        className="inline-block h-7 w-7 pr-2"
+                        viewBox="0 0 24 24">
+                        <path
+                        d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                    </svg>
+                    We just started our store and so far everything has been great. I couldn't be happier with the support and service they provide.
+                    <svg className="inline-block h-7 w-7 pl-2" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2.699 20c-.411 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.016.092-4.326-2.582-4.326-4.258 0-2.006 1.738-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.834 10.693-8.468 10.693zm10.833 0c-.41 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.015.092-4.326-2.582-4.326-4.258 0-2.006 1.739-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.833 10.693-8.468 10.693z" fillRule="nonzero"/></svg>
+                    </p>
+                    
+                </div>
                 <div className="mb-12 md:mb-0">
                     <div className="mb-6 flex justify-center">
                         <Image width={500} height={500} src="/misc/patel-blur-round.png" className="w-32 max-h-[131.33px] bg-cover bg-center rounded-full shadow-lg dark:shadow-black/30" alt='testimonial one person'/>
@@ -94,29 +109,7 @@ const Testimonials = () => {
                     <svg className="inline-block h-7 w-7 pl-2" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2.699 20c-.411 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.016.092-4.326-2.582-4.326-4.258 0-2.006 1.738-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.834 10.693-8.468 10.693zm10.833 0c-.41 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.015.092-4.326-2.582-4.326-4.258 0-2.006 1.739-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.833 10.693-8.468 10.693z" fillRule="nonzero"/></svg>
                     </p>
                 </div>
-                <div className="mb-1 md:mb-0">
-                    <div className="mb-6 flex justify-center">
-                    <Image width={100} height={100} src="/misc/alex-h-rounded.png" className="w-32 max-h-[131.33px] bg-cover bg-center rounded-full shadow-lg dark:shadow-black/30" alt='testimonial person three'/>
-                    </div>
-                    <h5 className="mb-4 text-xl font-semibold">Alex H.</h5>
-                    <p className="mb-4 text-lg">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        className="inline-block h-7 w-7 pr-2"
-                        viewBox="0 0 24 24">
-                        <path
-                        d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
-                    </svg>
-                    We just started our store and so far everything has been great. I couldn't be happier with the support and service they provide.
-                    <svg className="inline-block h-7 w-7 pl-2" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m2.699 20c-.411 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.016.092-4.326-2.582-4.326-4.258 0-2.006 1.738-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.834 10.693-8.468 10.693zm10.833 0c-.41 0-.699-.312-.699-.662 0-.249.145-.516.497-.703 1.788-.947 3.858-4.226 3.858-6.248-3.015.092-4.326-2.582-4.326-4.258 0-2.006 1.739-4.129 4.308-4.129 3.241 0 4.83 2.547 4.83 5.307 0 5.981-6.833 10.693-8.468 10.693z" fillRule="nonzero"/></svg>
-                    </p>
-                    
-                </div>
-
-                
-                
-                </div>
+            </div>
         </div>
     )
 }
