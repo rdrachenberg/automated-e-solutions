88a8dcb05fb68b7d73854d53520092689dfbe1a1
Ryan Drachenberg <tssinvestments@gmail.com>
2023-09-26 14:44:10 -0400

    fixed img alt descriptions

--- src/components/SocialLogos.js
+++ src/components/SocialLogos.js
@@ -6,22 +6,22 @@ const SocialLogos = () => {
         <div className='container mx-auto mt-1 justify-center items-center min-w-screen flex mb-1 '>
             <Link href={'https://www.youtube.com/@Aecomsolution/videos'} target='_blank'>
                 <div className='p-2 sm:p-6'>
-                    <Image width={125} height={125} src='/misc/youtube.png' alt='youtube comany logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
+                    <Image width={125} height={125} src='/misc/youtube.png' alt='youtube company logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
                 </div>
             </Link>
             <Link href={'/'} target='_blank'>
                 <div className='p-2 sm:p-6'>
-                    <Image width={125} height={125} src='/misc/instagram.png' alt='instagram comany logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
+                    <Image width={125} height={125} src='/misc/instagram.png' alt='instagram company logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
                 </div>
             </Link>
             <Link href={'/'} target='_blank'>
                 <div className='p-2 sm:p-6'>
-                    <Image width={125} height={125} src='/misc/facebook.png' alt='facebook comany logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
+                    <Image width={125} height={125} src='/misc/facebook.png' alt='facebook company logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
                 </div>
             </Link>
             <Link href={'https://www.tiktok.com/@automatedecominvestments?lang=en'} target='_blank'>
                 <div className='p-2 sm:p-6'>
-                    <Image width={125} height={125} src='/misc/tik-tok.png' alt='tiktok comany logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
+                    <Image width={125} height={125} src='/misc/tik-tok.png' alt='tiktok company logo' style={{ width: '100%', height: 'auto' }} className='rounded-full max-h-14'/>
                 </div>
             </Link>
         </div>
