851e2ef749915513ef85e226980fbf22e5ef4eab
Ryan Drachenberg <tssinvestments@gmail.com>
2023-09-30 18:20:20 -0400

    added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { Toaster } from 'react-hot-toast';
 import AppLayout from 'src/components/Layout'
 import FormProvider from 'src/context';
+import { Analytics } from '@vercel/analytics/react';
 import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <AppLayout>
         <Component {...pageProps} />
         <Toaster />
+        <Analytics />
       </AppLayout>
     </FormProvider>
     )
