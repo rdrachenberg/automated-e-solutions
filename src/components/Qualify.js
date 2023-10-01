29101c29b3d765a803c4dcce25483ecb92f5d0d8
Ryan Drachenberg <tssinvestments@gmail.com>
2023-10-01 11:59:50 -0400

    made adjustments to point-up.jpg

--- src/components/Qualify.js
+++ src/components/Qualify.js
@@ -15,7 +15,7 @@ const Qualify = () => {
                 </div>
             </div>
             <div className='mt-20'>
-                <Image width={500} height={500} src='/misc/point-up.jpg' alt="e-commerce girl pointing up" className='rounded-lg mx-auto shadow-xl' style={{width:'100%', height:'auto'}}/>
+                <Image width={200} height={200} src='/misc/point-up.jpg' alt="e-commerce girl pointing up" className='rounded-lg mx-auto shadow-xl max-w-md' style={{width:'auto', height:'auto'}}/>
             </div>
         </div>
     )
