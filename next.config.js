02a824718277ca6d0cd465b9f6ebc2d08f92c2b2
Ryan Drachenberg <tssinvestments@gmail.com>
2023-10-01 12:54:26 -0400

    added domain image support in next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [ 'https://assets.calendly.com/assets/external/widget.js', 'https://tecdn.b-cdn.net']
+    domains: [ 'https://assets.calendly.com/assets/external/widget.js', 'https://tecdn.b-cdn.net','https://aecomsolution.com', 'https://www.aecomsolution.com']
   }
 }
 
