84248abeaeab1ea009038f5823b18839df242fe5
Ryan Drachenberg <tssinvestments@gmail.com>
2023-10-05 18:51:31 -0400

    added globalThis support

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -4,10 +4,10 @@ let prisma;
 if(process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
 } else {
-    if(!global.prisma) {
-        global.prisma = new PrismaClient();
+    if(!globalThis.prisma) {
+        globalThis.prisma = new PrismaClient();
     }
-    prisma = global.prisma
+    prisma = globalThis.prisma
 }
 
 export default prisma
