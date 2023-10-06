6a727ba608a2608008f3051b1e8926b6a25509f6
Ryan Drachenberg <tssinvestments@gmail.com>
2023-10-05 20:13:55 -0400

    changed globalThis back to global for deployment

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -4,10 +4,10 @@ let prisma;
 if(process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
 } else {
-    if(!globalThis.prisma) {
-        globalThis.prisma = new PrismaClient();
+    if(!global.prisma) {
+        global.prisma = new PrismaClient();
     }
-    prisma = globalThis.prisma
+    prisma = global.prisma
 }
 
 export default prisma
