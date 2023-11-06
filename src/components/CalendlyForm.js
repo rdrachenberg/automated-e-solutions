f2e44259a03a64581607ef26e87cef46f30932b4
Ryan Drachenberg <tssinvestments@gmail.com>
2023-11-06 16:03:43 -0500

    fixed broken link in Calendly component

--- src/components/CalendlyForm.js
+++ src/components/CalendlyForm.js
@@ -3,7 +3,7 @@ import { InlineWidget } from "react-calendly";
 const CalendlyForm = () => {
     return (
         <div id='stepper'>
-            <InlineWidget url='https://calendly.com/d/2x6-dfk-s3z/30-minute-meeting' styles={{height: '1000px'}}/>
+            <InlineWidget url='https://calendly.com/automatedecominvestments/1-million-dollar-business' styles={{height: '1000px'}}/>
         </div>
     )
 }
---
+++ src/components/Transcript.js
@@ -0,0 +1,7 @@
+export const Transcript = () => {
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
